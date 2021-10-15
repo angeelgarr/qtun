9fd27778a54d4d895bc85b41fb0d5f25db133148
Alex <alex.tsai@protonmail.com>
2021-10-15 22:21:35 +0800

    update script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,14 +2,11 @@
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
 
-UNAME=$(uname)
+#Linux amd64
+GOOS=linux GOARCH=amd64 go build -o ./bin/qtun-linux-amd64 ./main.go
+#Linux arm64
+GOOS=linux GOARCH=arm64 go build -o ./bin/qtun-linux-arm64 ./main.go
+#Mac amd64
+GOOS=darwin GOARCH=amd64 go build -o ./bin/qtun-darwin-amd64 ./main.go
 
-if [ "$UNAME" == "Linux" ] ; then
-    GOOS=linux GOARCH=amd64 go build -o ./bin/qtun ./main.go
-elif [ "$UNAME" == "Darwin" ] ; then
-    GOOS=darwin GOARCH=amd64 go build -o ./bin/qtun ./main.go
-elif [[ "$UNAME" == CYGWIN* || "$UNAME" == MINGW* ]] ; then
-    GOOS=windows GOARCH=amd64 go build -o ./bin/qtun.exe ./main.go
-fi
-
-echo "done!"
+echo "DONE!!!"

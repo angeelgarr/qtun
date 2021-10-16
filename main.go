3e1937e1d56e7a39de63eb0a22c629766a8ec001
Alex <alex.tsai@protonmail.com>
2021-10-16 12:27:21 +0800

    add go:build

--- main.go
+++ main.go
@@ -1,3 +1,5 @@
+//go:build linux || darwin || windows
+
 package main
 
 import (
--- scripts/build.sh
+++ scripts/build.sh
@@ -8,5 +8,7 @@ GOOS=linux GOARCH=amd64 go build -o ./bin/qtun-linux-amd64 ./main.go
 GOOS=linux GOARCH=arm64 go build -o ./bin/qtun-linux-arm64 ./main.go
 #Mac amd64
 GOOS=darwin GOARCH=amd64 go build -o ./bin/qtun-darwin-amd64 ./main.go
+#Windows amd64
+GOOS=windows GOARCH=amd64 go build -o ./bin/qtun-windows-amd64.exe ./main.go
 
 echo "DONE!!!"
